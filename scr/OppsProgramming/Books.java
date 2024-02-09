d2b2b760cb90b29d31b96af740dc03f28b65483b
alphanse98 <alphanse98@gmail.com>
2024-02-09 16:08:58 +0530

    task done

--- scr/OppsProgramming/Books.java
+++ scr/OppsProgramming/Books.java
@@ -16,10 +16,7 @@ public class Books {
     }
 }
 
- class Library extends Books {//test
-
-    public static ArrayList<Object> BooksList = new ArrayList<Object>();
-
+ class Library extends Books {
     public static void main(String[] args) {
 
         addBook(1, "book one", "arun", true);
@@ -27,14 +24,18 @@ public class Books {
         addBook(3, "book three", "guru", true);
 
         removeBook(2);
+        searchBookById(1);
         displayBook();
     }
+
+     public static ArrayList<Object> BooksList = new ArrayList<Object>();
 
     // addBook create new book object then push in to new bookList array
    static Object addBook(int bookId,String  title,String  author,boolean isAvailable){
     return BooksList.add(new Books( bookId,  title,  author, isAvailable));
     }
 
+    //  removeBook update the status of the book isAvailable by  use by id
     static void removeBook(int id){
         for(int i =0;i<BooksList.size();i++){            
             if (((Books) BooksList.get(i)).bookId == id) {
@@ -46,6 +47,25 @@ public class Books {
         }
     }
 
+    // searchBookById search id
+     public static void searchBookById(int id) {
+         boolean found = false;
+         for (Object book : BooksList) {
+             if (((Books) book).bookId == id) {
+                 System.out.println("Book found:");
+                 System.out.println("Title: " + ((Books) book).title);
+                 System.out.println("Author: " + ((Books) book).author);
+                 System.out.println("Book ID: " + ((Books) book).bookId);
+                 found = true;
+                 break; // exit loop once book is found
+             }
+         }
+         if (!found) {
+             System.out.println("Book not found.");
+         }
+     }
+
+    //displayBook display all the book from BooksList using loop
     static void displayBook(){
         for (Object book : BooksList) {
             System.out.println("Book Title: " + ((Books) book).title);
